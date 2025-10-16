861e666868a54edbc3f59000c7677373fdc3d90e
dev <tmdals1704@gmail.com>
2025-10-16 18:27:23 +0900

    2510161827

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,57 @@
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthProvider } from './contexts/AuthContext';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { HomePage } from './pages/HomePage';
+import { AboutPage } from './pages/AboutPage';
+import { LoginPage } from './pages/LoginPage';
+import { SignupPage } from './pages/SignupPage';
+import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
+import { ResetPasswordPage } from './pages/ResetPasswordPage';
+import { SearchResultPage } from './pages/SearchResultPage';
+import { ReportPage } from './pages/ReportPage';
+import { AdminPage } from './pages/AdminPage';
+import { MyPage } from './pages/MyPage';
+import { CommunityPage } from './pages/CommunityPage';
+
+export function App() {
+  return (
+    <AuthProvider>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/community" element={<CommunityPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} />
+            <Route path="/search" element={<SearchResultPage />} />
+            <Route path="/report" element={<ReportPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+          </Routes>
+        </main>
+        <Footer />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </div>
+    </AuthProvider>
+  );
+}
+
+
